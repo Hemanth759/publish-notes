ca5bf051f1da338291e671cd34a963102a1db01e
Vanam Hemanth <hemanthvanam@gmail.com>
2025-03-12 21:28:52 +0530

    vault auto backup: 2025-03-12 21:28:52 - MacBook

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Hemanth's Notes",
+    pageTitle: "Hemanth's Digital Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
